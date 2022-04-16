b79bb001d283d71f4866872c2c8ab7bf8a08f62e
ypetit <public@ypetit.net>
2022-04-16 10:57:47 +0200

    feat: use multistep build to download models separately and assemble them in the end

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,4 +11,8 @@ echo -e "\tBUILD Docker image ${CONFIG[name]}:${CONFIG[version]} !"
 echo -e "********************************************************************************\n\n"
 
 # ${1} is used to insert some parameters to build command like --no-cache
+DOCKER_BUILDKIT=1 docker build ${1} -t "${CONFIG[name-base]}:${CONFIG[version]}" -f "${_DIR}/../docker/Dockerfile-base" "${_DIR}/../docker"
+DOCKER_BUILDKIT=1 docker build ${1} -t "${CONFIG[name-model-fr]}:${CONFIG[version]}" -f "${_DIR}/../docker/Dockerfile-model-fr" "${_DIR}/../docker"
+DOCKER_BUILDKIT=1 docker build ${1} -t "${CONFIG[name-model-es]}:${CONFIG[version]}" -f "${_DIR}/../docker/Dockerfile-model-es" "${_DIR}/../docker"
+DOCKER_BUILDKIT=1 docker build ${1} -t "${CONFIG[name-model-en]}:${CONFIG[version]}" -f "${_DIR}/../docker/Dockerfile-model-en" "${_DIR}/../docker"
 DOCKER_BUILDKIT=1 docker build ${1} -t "${CONFIG[name]}:${CONFIG[version]}" "${_DIR}/../docker"
