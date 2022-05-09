ba3cf2baa0905eb5f0ff94db9609c2da688437cd
Christophe Labedan <487306+plugandplay@users.noreply.github.com>
2022-05-09 16:50:00 +0200

    chore: build an image without gpu

---
+++ docker/resources/gunicorn.conf.py
@@ -0,0 +1,8 @@
+bind = ["127.0.0.1:8000"]
+accesslog = "/home/codatalab/access.log"
+errorlog = "/home/codatalab/error.log"
+workers = 1
+threads = 1
+daemon = True
+chdir = "scripts"
+reload = True
